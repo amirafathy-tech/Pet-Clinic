ed7fa3364e33d7f23ba8ae8e42529dc071b3b2ed
amirafathy-tech <amira.fathy@solex.tech>
2023-11-28 17:58:57 +0200

    refactoring to multimodule maven project. Closes #1.

---
+++ Pet-Clinic/pet-clinic-web/src/test/java/org/example/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package org.example.petclinic;
+
+import org.example.petclinic.PetClinicApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+//@SpringBootTest(classes = PetClinicApplicationTests.class)
+//@ContextConfiguration(classes = PetClinicApplication.class)
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
