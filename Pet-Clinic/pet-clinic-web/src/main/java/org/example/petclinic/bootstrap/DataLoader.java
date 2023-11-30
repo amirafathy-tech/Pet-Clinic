1896205bab96791be972c97f1e609f1a00d8c329
amirafathy-tech <amira.fathy@solex.tech>
2023-11-30 15:52:21 +0200

    Refactored Map Impl to abstract ID generation, Closes #23

--- Pet-Clinic/pet-clinic-web/src/main/java/org/example/petclinic/bootstrap/DataLoader.java
+++ Pet-Clinic/pet-clinic-web/src/main/java/org/example/petclinic/bootstrap/DataLoader.java
@@ -23,7 +23,6 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Amira");
         owner1.setLastName("Fathy");
 
@@ -31,7 +30,6 @@ public class DataLoader implements CommandLineRunner {
 
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Amira2");
         owner2.setLastName("Fathy2");
 
@@ -42,14 +40,12 @@ public class DataLoader implements CommandLineRunner {
 
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessi");
         vet2.setLastName("Porter");
 
