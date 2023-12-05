b6bbd8065c6d972d09cacb5679f084ca4b7da640
amirafathy-tech <amira.fathy@solex.tech>
2023-12-05 17:23:09 +0200

    Added spring data jpa service for vet. Closes #41

--- Pet-Clinic/pet-clinic-data/src/main/java/org/example/petclinic/services/VetService.java
+++ Pet-Clinic/pet-clinic-data/src/main/java/org/example/petclinic/services/VetService.java
@@ -2,8 +2,7 @@ package org.example.petclinic.services;
 
 import org.example.petclinic.model.Vet;
 
-import java.util.Set;
-
 public interface VetService extends CrudService<Vet,Long>{
 
+    void deleteByID(Long aLong);
 }
