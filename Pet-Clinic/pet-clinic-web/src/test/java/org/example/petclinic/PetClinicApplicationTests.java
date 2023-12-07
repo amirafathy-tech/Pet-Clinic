4bf2712543c86ba5bc962f25e747d30de93d1017
amirafathy-tech <amira.fathy@solex.tech>
2023-12-07 14:51:41 +0200

    Upgraded to Junit 5. Closes #73

--- Pet-Clinic/pet-clinic-web/src/test/java/org/example/petclinic/PetClinicApplicationTests.java
+++ Pet-Clinic/pet-clinic-web/src/test/java/org/example/petclinic/PetClinicApplicationTests.java
@@ -1,10 +1,11 @@
 package org.example.petclinic;
 
-import org.example.petclinic.PetClinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 //@SpringBootTest(classes = PetClinicApplicationTests.class)
 //@ContextConfiguration(classes = PetClinicApplication.class)
